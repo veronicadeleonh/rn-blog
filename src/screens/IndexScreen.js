e4a9d5b7b903e11b20294fe0880aa9530a9b297c
Veronica De Leon <v.dleon@gmail.com>
2021-01-25 17:19:53 +0100

    context set up

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, Text, StyleSheet } from "react-native"
+
+const IndexScreen = () => {
+    return <View>
+        <Text>Index Screen</Text>
+    </View>
+}
+
+export default IndexScreen
