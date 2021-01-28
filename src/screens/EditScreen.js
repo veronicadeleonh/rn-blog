d8f5b1159d1c8b06d8e4ad0f339901a57d359243
Veronica De Leon <v.dleon@gmail.com>
2021-01-28 13:54:02 +0100

    refactoring CreateScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,18 +1,17 @@
-import React, { useState, useContext } from "react"
-import { View, Text, StyleSheet } from "react-native"
+import React, { useContext } from "react"
+import { StyleSheet } from "react-native"
 import { Context } from "../context/BlogContext"
+import BlogPostForm from "../components/BlogPostFrom"
 
 const EditScreen = ({ navigation }) => {
 
-    console.log(navigation.getParam("id"))
+    // console.log(navigation.getParam("id"))
 
     const { state } = useContext(Context) 
 
     const blogPost = state.find(blogPost => blogPost.id === navigation.getParam("id"))
 
-    return <View>
-        <Text>Edit Screen - {navigation.getParam("id")}</Text>
-    </View>
+    return <BlogPostForm />
 }
 
 const styles = StyleSheet.create({})
