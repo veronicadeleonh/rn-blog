1fada4646960301a651aac4552a94c034eb24ef6
Veronica De Leon <v.dleon@gmail.com>
2021-01-28 13:05:44 +0100

    cannot pass id to EditScreen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,9 +1,17 @@
-import React from "react"
+import React, { useState, useContext } from "react"
 import { View, Text, StyleSheet } from "react-native"
+import { Context } from "../context/BlogContext"
 
-const EditScreen = () => {
+const EditScreen = ({ navigation }) => {
+
+    console.log(navigation.getParam("id"))
+
+    const { state } = useContext(Context) 
+
+    const blogPost = state.find(blogPost => blogPost.id === navigation.getParam("id"))
+
     return <View>
-        <Text>Edit Screen</Text>
+        <Text>Edit Screen - {navigation.getParam("id")}</Text>
     </View>
 }
 
